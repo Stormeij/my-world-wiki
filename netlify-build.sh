f6437814a433cd3b058bce240dec769cd51534e0
Stormeij <stormeij@mail.ru>
2025-07-22 23:58:13 +0700

    Fix: убран перезаписывающий index.html

--- netlify-build.sh
+++ netlify-build.sh
@@ -10,8 +10,4 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 # Сборка сайта
-python -m mkdocs build --site-dir public || true
-
-# Гарантированное создание папки public
-mkdir -p public
-echo "<h1>Build completed</h1>" > public/index.html
+python -m mkdocs build --site-dir public
