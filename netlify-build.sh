b8115e12cb30c5088e2540db6e3ece2628adfeda
Stormeij <stormeij@mail.ru>
2025-11-08 01:14:12 +0700

    обновление

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-# Проверка версии Python
-echo "Using Python: $(python --version)"
-echo "Using PIP: $(pip --version)"
-
 # Установка зависимостей
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 # Сборка сайта
-python -m mkdocs build --site-dir public
+python -m mkdocs build 
